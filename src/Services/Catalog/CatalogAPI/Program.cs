923068e36fba76e5ce4ad5b1fcdf5b99c6e8a0fa
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-02-28 15:09:27 +0200

    add catalog service

---
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add DI - AddServices to the contianer
+
+var app = builder.Build();
+
+//Configure the HTTP request pilpeline - UseMethod
+
+app.Run();
