321c14d467f0065c08801098c5c38cf9386380c9
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-03-09 22:54:24 +0200

    add Carter and MediatR configurations

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add DI - AddServices to the contianer
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 //Configure the HTTP request pilpeline - UseMethod
-
+app.MapCarter();
 app.Run();
