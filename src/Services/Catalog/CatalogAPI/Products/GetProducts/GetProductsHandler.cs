a0c35729647c316acbf83afeb2bae19df2029549
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-03-15 14:36:30 +0200

    add get products feature

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQuery> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductsQueryHandler.Handle called with Query: {query}");
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken).ConfigureAwait(false);
+            
+            return new GetProductsResult(products);
+        }
+    }
+}
