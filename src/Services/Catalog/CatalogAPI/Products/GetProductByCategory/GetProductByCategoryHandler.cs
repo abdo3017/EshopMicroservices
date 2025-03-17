9e0e44f5ee114ecac1a85326588a160a74f07e73
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-03-17 13:56:00 +0200

    add new api "get products by category"

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CatalogAPI.Products.GetProductsByCategory
+{
+
+    public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+    public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+
+    public class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQuery> logger) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+    {
+
+        public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductsByCategoryQueryHandler.Handle called with Query: {query}");
+
+            var products = await session.Query<Product>().Where( p=>p.Category.Contains(query.Category)).ToListAsync(cancellationToken).ConfigureAwait(false);
+            
+            return new GetProductsByCategoryResult(products);
+        }
+    }
+}
