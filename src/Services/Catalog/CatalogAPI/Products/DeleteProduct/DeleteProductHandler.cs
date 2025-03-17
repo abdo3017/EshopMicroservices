69912c2c21d4239c5fcff2c3cebc7f72d1960e3f
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-03-17 14:36:50 +0200

    add api "delete product"

---
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace CatalogAPI.Products.DeleteProduct
+{
+
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+
+    public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommand> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"DeleteProductCommandHandler.Handle called with Command: {command}");
+
+            var product = await session.LoadAsync<Product>(command.Id,cancellationToken).ConfigureAwait(false);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            session.Delete(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+        }
+    }
+}
