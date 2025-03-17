cfb7701e6eaa3de3b8f0202043dbdb2e0287a657
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-03-17 13:04:54 +0200

    add new api "get product by id"

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Products);
+
+
+    public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQuery> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductByIdQueryHandler.Handle called with Query: {query}");
+
+            var product = await session.LoadAsync<Product>(query.Id,cancellationToken).ConfigureAwait(false);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
