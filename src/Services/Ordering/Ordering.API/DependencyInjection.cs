ac9373e25ad2ac41c8134f10b31386ff7fd100e3
abdelrahman.ghazi <abdelrahman.ghazi@cyshield.com>
2025-05-24 19:33:41 +0300

    add DI base

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            return app;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,17 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 // Configure the HTTP request pipeline.
 
-
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 
+app.UseApiServices();
 
 app.Run();
